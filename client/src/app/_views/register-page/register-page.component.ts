7c6563354fc0108cb4a63a367095d7517dd1f547
KamiLuc <kamil.luc168@protonmail.com>
2024-03-08 18:08:35 +0100

    Add register page

--- client/src/app/_views/register-page/register-page.component.ts
+++ client/src/app/_views/register-page/register-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-register-page',
-  templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.scss']
+	selector: 'app-register-page',
+	templateUrl: './register-page.component.html',
+	styleUrls: ['./register-page.component.scss'],
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements OnInit {
+	model: any = {};
 
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	register() {
+		console.log(this.model);
+	}
+
+	cancel() {
+		console.log('canceled');
+	}
 }
